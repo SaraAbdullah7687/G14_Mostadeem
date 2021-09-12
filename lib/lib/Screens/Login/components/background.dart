2d8e7feb602814c51d2f31845a1899c1ac955e3e
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-09-12 11:18:38 +0300

    Add files via upload

---
+++ lib/lib/Screens/Login/components/background.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height,
+      child: Stack(             // add riyadh landmarks in this file
+        alignment: Alignment.center,
+        children: <Widget>[
+
+          Positioned(
+          top:0,
+          width:size.width,
+          child: Image.asset('assets/images/main_top.png'),),
+          Positioned(
+            bottom:0,
+            width:size.width,
+            child: Image.asset('assets/images/main_bottom.png'),),
+         /* Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/main_top.png",
+              width: size.width * 0.35,
+            ),
+          ),*/
+          /*
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset(
+              "assets/images/login_bottom.png",
+              width: size.width * 0.4,
+            ),
+          ),*/
+          child,
+        ],
+      ),
+    );
+  }
+}
