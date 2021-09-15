1dbcaeb0f950f41e92b77b8f51e540272dbfe6b8
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-09-15 07:52:50 +0300

    Add files via upload

--- lib/lib/Screens/Welcome/components/body.dart
+++ lib/lib/Screens/Welcome/components/body.dart
@@ -1,38 +1,87 @@
-// ignore: unused_import
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:mostadeem_app/Screens/background.dart';
-import 'package:mostadeem_app/constants.dart';
-import 'package:mostadeem_app/main.dart';
+import 'package:flutter_auth/Screens/Login/login_screen.dart';
+import 'package:flutter_auth/Screens/Signup/signup_screen.dart';
+import 'package:flutter_auth/Screens/Welcome/components/background.dart';
+import 'package:flutter_auth/components/rounded_button.dart';
+import 'package:flutter_auth/constants.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
-  const Body({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size; 
-    //This size provides us with total height and width of our screen
-    // ignore: sized_box_for_whitespace, prefer_const_constructors
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
     return Background(
-      // ignore: prefer_const_literals_to_create_immutables
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-         
-         const SizedBox(height: 16),
-         ElevatedButton(style: ElevatedButton.styleFrom(),
-         child: Text('Login'),
-         onPressed: (){},
-
-
-
-         )
-
-          
-        ]
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            
+           // SizedBox(height: size.height * 0.05),
+           /* Positioned(
+            left:0,
+            top: 190,
+            width:size.width,
+            child: Image.asset('assets/images/logo.png'),),*/
+           /* SvgPicture.asset(
+              "assets/icons/chat.svg", // change it to mostadeem
+              height: size.height * 0.45,
+            ),*/
+             AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_outlined, color: Colors.green[900]),
+          onPressed: () {
+            // passing this to our root
+            Navigator.of(context).pop();
+          },
         ),
-        );
+      ),
+
+             SizedBox(
+             height: 250, // change the number to make the logo bigger     
+           // width:size.width,
+             child: Image.asset("assets/images/logo.png",
+              fit: BoxFit.contain,)),
+SizedBox(height: size.height * 0.05),
+            Text(
+              "SIGN UP AS",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.05), // for what?
+            RoundedButton(
+              text: "Contirbutor", // change size + font
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen(); // reg form contirbutor
+                    },
+                  ),
+                );
+              },
+            ),
+            RoundedButton(
+              text: "Institution",
+              color: Color(0XFFEDE7CD),
+              textColor: Colors.green[900], // Colors.black,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen(); // reg form Institution
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
-
+// must add back button or allredy have an account
