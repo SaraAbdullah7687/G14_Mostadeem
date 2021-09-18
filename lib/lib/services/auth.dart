f660d676af7d44b55fe808c13cae1a6bee6054e9
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-09-18 21:03:10 +0300

    Add files via upload

--- lib/lib/services/auth.dart
+++ lib/lib/services/auth.dart
@@ -1,3 +1,4 @@
+import 'package:test_project/models/ContributorModel.dart';
 import 'package:test_project/models/userMu.dart';
 import 'package:test_project/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,6 +9,7 @@ class AuthService {
 
   // create user obj based on firebase user
   UserMu _userFromFirebaseUser(User user) { // was FirebaseUser instead of User
+  print(user); // maybe wrong
     return user != null ? UserMu(uid: user.uid) : null;
   }
 
@@ -19,6 +21,7 @@ class AuthService {
   }
 
   // sign in anon
+  /*
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously(); // was AuthResult instead of UserCredential
@@ -28,14 +31,14 @@ class AuthService {
       print(e.toString());
       return null;
     }
-  }
+  }*/
 
   // sign in with email and password
   Future signInWithEmailAndPassword(String email, String password) async {
     try {
-      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
-      User user = result.user; // was FirebaseUser instead of User
-      return user;
+      UserCredential _result = await _auth.signInWithEmailAndPassword(email: email.trim(), password: password);
+      User _user = _result.user; // was FirebaseUser instead of User
+      return _user;
     } catch (error) {
       print(error.toString());
       return null;
@@ -43,13 +46,28 @@ class AuthService {
   }
 
   // register with email and password
-  Future registerWithEmailAndPassword(String email, String password) async {
-    try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-      User user = result.user; // was FirebaseUser instead of User
+  Future registerWithEmailAndPassword(String email, String password, String name) async { // خلي الميثود تقبل رقم جواله
+   String retVal = "retVal error";
+   try {
+     print("before registering user");
+      UserCredential _result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User _user = _result.user; // was FirebaseUser instead of User
+      print("user registered");
+       ContributorModel _userCont = ContributorModel(
+        uid: _user.uid,
+        email: _user.email,
+        name: name,); //notifToken: await _fcm.getToken(),?? 
+
+        String _returnString = await DatabaseService().createUser(_userCont); // maybe database service
+        print("user created");
+        if (_returnString == "success") { // didn't work
+        retVal = "success";
+      }
+    print(retVal); // delete it 
+       
       // create a new document for the user with the uid
-     // await DatabaseService(uid: user.uid).updateUserData('0','new crew member', 100);
-      return _userFromFirebaseUser(user);
+     // await DatabaseService(uid: _user.uid).updateUserData('0','new crew member', 100);
+      return _userFromFirebaseUser(_user);
     } catch (error) {
       print(error.toString());
       return null;
--- lib/lib/services/database.dart
+++ lib/lib/services/database.dart
@@ -1,15 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:test_project/models/ContributorModel.dart';
 
 class DatabaseService {
 
-  final String uid;
-  DatabaseService({ this.uid });
+  //final String uid;
+ //DatabaseService({ this.uid });
 
   // collection reference
-  final CollectionReference userCollection = FirebaseFirestore.instance.collection('users'); // was Firestore instead of FirebaseFirestore
+  final CollectionReference _userCollection = FirebaseFirestore.instance.collection('users'); // was Firestore instead of FirebaseFirestore
 /*
-  Future<void> updateUserData(String sugars, String name, int strength) async {
-    return await userCollection.doc(uid).setData({ // was document instead of doc
+  Future<void> updateUserData(String name, String email) async {
+    return await userCollection.doc(uid).set({ // was document instead of doc
       'sugars': sugars,
       'name': name,
       'strength': strength,
@@ -18,8 +19,32 @@ class DatabaseService {
 
   // get brews stream
   Stream<QuerySnapshot> get brews {
-    return userCollection.snapshots();
+    return _userCollection.snapshots();
   }
+
+
+
+Future<String> createUser(ContributorModel user) async {
+    String retVal = "error";
+//ممكن طريقة انشاء الكولكشن غلط مدري كم عندنا من كوليكشن
+    try {
+      await _userCollection.doc(user.uid).set({
+        'Name': user.name,
+        'email': user.email,
+        //'notifToken': user.notifToken,?? what should we add?
+      });
+      retVal = "success";
+    } catch (e) {
+      print(e);
+    }
+
+    return retVal;
+  }
+
+
+
+
+
 
 }
 // modify this class from the bookclub
