ca3351a60d428e6d228616ff8781522e076493cf
nouf21 <90301568+nouf21@users.noreply.github.com>
2021-09-19 22:56:22 +0300

    Add files via upload

---
+++ institution_registration/lib/reg.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+
+/*class background extends StatelessWidget {
+  const background({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+     Size size = MediaQuery.of(context).size; 
+
+    return Container(
+      color: Colors.white,
+      height:size.height,
+      width: double.infinity,
+
+      child: Stack(
+        children: <Widget>[
+          
+        Positioned(
+        child: 
+        Image.asset('assets/images/main_top.png'),),
+
+        Positioned(
+        top:130 ,
+        left: 100,
+        width:200,
+        child: 
+        Image.asset('assets/images/logo.png'),),
+
+        Positioned(
+        bottom: 0,
+        width:size.width,
+        child: Image.asset('assets/images/main_bottom.png'),)
+
+        ,Positioned(child:
+        TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter your username',
+            ),)),
+        ]),
+    );
+      
+    
+  }
+  
+}*/
+
+
+
