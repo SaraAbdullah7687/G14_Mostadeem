ebaac97f162f1332b2c645f04eb01823588cb839
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-09-24 01:58:12 +0300

    Add files via upload

--- lib/lib/Admin/viewInstitution.dart
+++ lib/lib/Admin/viewInstitution.dart
@@ -4,11 +4,15 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 //import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:test_project/Screens/background.dart';
 import 'package:test_project/services/auth.dart';
 import 'package:test_project/shared/loading.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'components/social_icon.dart';
 
 class ViewInstitution extends StatelessWidget {
   final AuthService _auth = AuthService();
@@ -150,21 +154,27 @@ class ViewInstitution extends StatelessWidget {
                       children: <Widget>[
                         
                           Container(
-                          width: 100,
-                          height: 100,
-                          child: ClipRRect(
+                            margin: EdgeInsets.only(top:15),
+                          width: 150,
+                         // height: 100,
+                          child: Column( children: <Widget>[
+                          ClipRRect(
                             borderRadius: new BorderRadius.circular(24.0),
-                            child: Image.asset("assets/images/logo.png",), /*Image(
+                            child: //Image.asset("assets/images/logo.png",), 
+                            Image(
                               fit: BoxFit.contain,
-                              alignment: Alignment.topRight,
-                              image: NetworkImage(
-                                  "https://images.unsplash.com/photo-1495147466023-ac5c588e2e94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"),
-                            ),*/
-                          ),),
+                              alignment: Alignment.topLeft,
+                              image: AssetImage("assets/images/logo.png",),
+                            ),
+                          ),
+                          Spacer(),
+                          contactIcons(context,document),
+                          
+                          ],),),
 
                         Container(
                           child: Padding(
-                            padding: const EdgeInsets.only(right: 50.0),
+                            padding: const EdgeInsets.only(right: 90.0),
                             child: myDetailsContainer1(context,document),
                           ),
                         ),
@@ -182,82 +192,133 @@ class ViewInstitution extends StatelessWidget {
      // mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        
+        /*
         Container( child: Padding(
           padding: EdgeInsets.only(right:60.0),
-          child: Container(child:  Text(document['name'],
-            style: TextStyle(color: Colors.green[900], fontSize: 30.0,fontWeight: FontWeight.bold),),)),
-        ),Spacer(),
+          child:*/
+           Container( margin: EdgeInsets.symmetric(vertical:22,horizontal: 0,), child:  Text(document['name'],
+            style: TextStyle(color: Colors.green[900], fontSize: 30.0,fontWeight: FontWeight.bold),),),//),
+       // ),
+       // Spacer(),
         Container(child: Text(document['category'],
           style: TextStyle(color: Colors.black54, fontSize: 18.0,fontWeight: FontWeight.bold),)),
-           Spacer(),/*
-Row( 
-  children: <Widget>[
-  
-          Container(
-            child: Text(document['email'],
-          style: TextStyle(color: Colors.black54, fontSize: 18.0,fontWeight: FontWeight.bold),)),
-         /* Container(
-           // margin: EdgeInsets.only(left:10.0),
-            child: Text(document['name'],
-          style: TextStyle(color: Colors.black54, fontSize: 18.0,fontWeight: FontWeight.bold),)),*/
-],), */],
+           Spacer(),
+// add Commercial record
+
+        Container(
+          margin: EdgeInsets.only(left:60),
+           width:100,
+           height: 60,
+          child:  
+        Row(
+          children: <Widget>[
+            Flexible(  child:
+GestureDetector( child:
+IconButton(
+  iconSize: 50.0,
+          icon: const Icon(Icons.check),
+          color: Colors.lightGreen[600],
+          onPressed: (){ },), // pop up , are you show?
+),),
+Flexible(  child:
+GestureDetector( child:
+IconButton(
+  iconSize: 50.0,
+          icon: const Icon(Icons.clear),
+          color: Colors.red[800],
+          onPressed: (){ },), // pop up , are you show?
+),),
+          ],  
+        ),
+        ),
+  ],
     );
 
  }
-          /*    
-       new Padding(
-            padding: const EdgeInsets.all(16.0),
-            child:Container(
-      child: Card(
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-         elevation: 8, // ظل حول الكارد
-         //shadowColor: Colors.green,
-         shape:  OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10), 
-              borderSide: BorderSide(color: Colors.white), //BorderSide(color: Colors.green, width: 1)
-          ),
-              
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
-                child: Row(children: <Widget>[
-                  Text(document['name'], style: new TextStyle(fontSize: 30.0),),
-                  Spacer(),
-                ]),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 4.0, bottom: 80.0),
-                child: Row(children: <Widget>[
-                  
-                    Text(document['email'], style: new TextStyle(fontSize: 30.0),),  
-                  Spacer(),
-                ]),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-                child: Row(
-                  children: <Widget>[
-                    Text(document['category'], style: new TextStyle(fontSize: 30.0),),  
-                  Spacer(),
-                    Icon(Icons.foundation_rounded),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    ),);*/
+       
+Widget contactIcons(BuildContext context, DocumentSnapshot document){
+  return Container(
+    margin: EdgeInsets.only(left:15),
+     child:Row( 
+  children: <Widget>[
+    Flexible( child:
+            IconButton(
+          icon: const Icon(Icons.mail_outline),
+          color: Colors.lightGreen[900],
+          tooltip: 'Send email',
+          onPressed: ()=> _sendingMails(document.get("email")),
+          
+          ),),
+    Flexible( child:
+          SocalIcon(
+                  iconSrc: "assets/icons/twitter.svg",
+                  color: Colors.lightGreen[900],
+                  press: ()=> _goToTwitter(document.get("twitter")),
+                ),),
+
+    Flexible( child:
+                IconButton(
+          icon: const Icon(Icons.phone),
+          color: Colors.lightGreen[900],
+          onPressed: ()=> _goToWhatsapp(document.get("phone")),),),
+  ],),);
+}
+
+
+Future _sendingMails(String email) async {
+  String url = 'mailto:$email'; // specify mail from snapchot
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
+}
+  
+
+Future _goToTwitter(String account) async {
+  String url = 'https://twitter.com/$account';
+if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+Future _goToWhatsapp(String phone) async {
+String url ="whatsapp://send?phone=$phone"; // maybe needs to be modified to api?
+if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 
 
+Future<void> _showMyDialog(String status) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Institution Status'),
+        content: const Text('Do you want to'),
+        actions: <Widget>[
+          TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('Yes'),
+            ),
+          ],
+        //),
+      );
+    },
+  );
+}
 
-
-
+}// end of class
 
 
 
