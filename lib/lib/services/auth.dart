54c9b08bb7f601c8252e6af979ddac0392c7b86f
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-09-23 15:40:54 +0300

    Add files via upload

--- lib/lib/services/auth.dart
+++ lib/lib/services/auth.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:test_project/models/ContributorModel.dart';
 import 'package:test_project/models/userMu.dart';
 import 'package:test_project/services/database.dart';
@@ -34,26 +35,32 @@ class AuthService {
   }*/
 
   // sign in with email and password
-  Future signInWithEmailAndPassword(String email, String password) async {
+  Future <dynamic> signInWithEmailAndPassword(String email, String password) async {
     try {
       UserCredential _result = await _auth.signInWithEmailAndPassword(email: email.trim(), password: password);
       User _user = _result.user; // was FirebaseUser instead of User
       return _user;
-    } catch (error) {
-      print(error.toString());
-      return null;
-    } 
+    }  on FirebaseAuthException catch (e) {
+  if (e.code == 'user-not-found') {
+    print('No user found for that email.');
+    return "No-user-found"; // maybe change it 
+  } else if (e.code == 'wrong-password') {
+    print('Wrong password provided for that user.');
+    return "Wrong-password"; // maybe change it 
+  }
+} 
   }
 
   // register with email and password
-  Future registerWithEmailAndPassword(String email, String password, String name) async { // خلي الميثود تقبل رقم جواله
+  Future <dynamic>registerWithEmailAndPassword(String email, String password, String name) async { // خلي الميثود تقبل رقم جواله
    String retVal = "retVal error";
    try {
      print("before registering user");
       UserCredential _result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+     await _auth.currentUser.reload(); //delete it
       User user = _result.user; // was FirebaseUser instead of User
       print("user registered");
-      /* ContributorModel _userCont = ContributorModel(
+       ContributorModel _userCont = ContributorModel(
         uid: user.uid,
         email: user.email,
         name: name,); //notifToken: await _fcm.getToken(),?? 
@@ -62,15 +69,28 @@ class AuthService {
         print("user created");
         if (_returnString == "success") { // didn't work
         retVal = "success";
-      }*/
+        print("retVal success");
+      }
    // print(retVal); // delete it 
        
       // create a new document for the user with the uid
      // await DatabaseService(uid: _user.uid).updateUserData('0','new crew member', 100);
       return _userFromFirebaseUser(user);
-    } catch (error) {
-      print(error.toString());
-      return null;
+    } on FirebaseAuthException catch (signUpError) {
+      print("email exists");
+      //if (signUpError is PlatformException){
+        switch(signUpError.code){
+          case "ERROR_EMAIL_ALREADY_IN_USE": // ما تضبط
+          print("email exists 1 case");
+          return "email exists";
+          break;
+
+          case "email-already-in-use":
+          print("email exists 2 case");
+          return "email exists";
+          break;
+        
+      }
     } 
   }
 
@@ -83,5 +103,17 @@ class AuthService {
       return null;
     }
   }
+
+  //get the current user
+   Future<User> getCurrentUser() async {
+    return _auth.currentUser;
+  }
+
+  //get current user ID
+   String getCurrentUserID() {
+    return _auth.currentUser.uid;
+  }
+
+
 
 }
