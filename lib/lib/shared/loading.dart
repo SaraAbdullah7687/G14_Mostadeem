c73cb35f1b2b840478772e9f005936f3b6492b3c
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-09-28 21:13:59 +0300

    Add files via upload

--- lib/lib/shared/loading.dart
+++ lib/lib/shared/loading.dart
@@ -5,10 +5,10 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green[50],
+      color: Colors.white,
       child: Center(
         child: SpinKitChasingDots(
-          color: Colors.green[300],
+          color: Color.fromRGBO(103, 145, 61, 1),
           size: 50.0,
         ),
       ),
