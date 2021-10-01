537feb20b47300132f0d41243cc77d930c85122e
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-10-02 01:34:44 +0300

    Add files via upload

--- lib/lib/components/already_have_an_account_acheck.dart
+++ lib/lib/components/already_have_an_account_acheck.dart
@@ -12,24 +12,27 @@ class AlreadyHaveAnAccountCheck extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          login ? "Don’t have an Account ? " : "Already have an Account ? ",
-          style: TextStyle(color: kPrimaryColor),
-        ),
-        GestureDetector(
-          onTap: press,
-          child: Text(
-            login ? "Sign Up" : "Sign In",
-            style: TextStyle(
-              color: kPrimaryColor,
-              fontWeight: FontWeight.bold,
-            ),
+    return Container(
+      margin: EdgeInsets.only(bottom: 6),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            login ? "Don’t have an Account ? " : "Already have an Account ? ",
+            style: TextStyle(color: kPrimaryColor),
           ),
-        )
-      ],
+          GestureDetector(
+            onTap: press,
+            child: Text(
+              login ? "Sign Up" : "Sign In",
+              style: TextStyle(
+                color: kPrimaryColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
