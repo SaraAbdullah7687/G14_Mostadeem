35c76a3eabcf8b426f21accb28dc77212b82d95d
Afnan Alzuayr <90301688+Afn4nz@users.noreply.github.com>
2021-10-02 03:15:19 +0300

    Update calendar.dart

--- lib/screens/calendar.dart
+++ lib/screens/calendar.dart
@@ -1,5 +1,5 @@
 // ignore: file_names
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
@@ -257,4 +257,274 @@ class HeaderWidget extends StatelessWidget {
           child,
         ],
       );
+}*/
+
+/* The new calender with sperate time and date*/
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'home3.dart';
+import 'locatin.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Date & Time';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+        ),
+        home: MainPage(
+          category: '',
+        ),
+
+        ///PARAMETER
+      );
+}
+
+class MainPage extends StatefulWidget {
+  String category; //FINAL ?
+
+  //constructer--------------------------------------------------------------------------------------------
+  MainPage({required this.category});
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Date & Time'),
+            backgroundColor: Color.fromRGBO(48, 126, 80, 1),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(18),
+              ),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(
+                  Icons.arrow_forward_rounded,
+                  color: Colors.white,
+                ),
+                tooltip: 'Show Snackbar',
+                onPressed: () {
+                  /*   Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => LocationApp(
+                              category: widget.category,
+                              dateTime:
+                                  dateTime, // ERROR HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEERRREEEEEEEEEEEEEEE
+                            )),
+                  );*/
+                },
+              ),
+            ],
+            leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back_rounded,
+                color: Colors.white,
+              ),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => home3()),
+                );
+              },
+            ),
+            toolbarHeight: 50.0,
+          ),
+          //      backgroundColor: Colors.white10,
+          body: Padding(
+            padding:
+                const EdgeInsets.only(left: 40.0, right: 40.0, bottom: 100),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TimePickerWidget(),
+                DatePickerWidget(),
+              ],
+            ),
+          ),
+        ),
+      );
+}
+
+class DatePickerWidget extends StatefulWidget {
+  @override
+  _DatePickerWidgetState createState() => _DatePickerWidgetState();
+}
+
+class _DatePickerWidgetState extends State<DatePickerWidget> {
+  DateTime? date;
+
+  String getText() {
+    if (date == null) {
+      return 'Select Date';
+    } else {
+      return DateFormat('MM/dd/yyyy').format(date!);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => ButtonHeaderWidget(
+        title: 'Date',
+        text: getText(),
+        onClicked: () => pickDate(context),
+      );
+
+  Future pickDate(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+      context: context,
+      initialDate: date ?? initialDate,
+      firstDate: DateTime(DateTime.now().year - 5),
+      lastDate: DateTime(DateTime.now().year + 5),
+    );
+
+    if (newDate == null) return;
+
+    setState(() => date = newDate);
+  }
+}
+
+class TimePickerWidget extends StatefulWidget {
+  @override
+  _TimePickerWidgetState createState() => _TimePickerWidgetState();
+}
+
+class _TimePickerWidgetState extends State<TimePickerWidget> {
+  TimeOfDay? time;
+
+  String getText() {
+    if (time == null) {
+      return 'Select Time';
+    } else {
+      final hours = time!.hour.toString().padLeft(2, '0');
+      final minutes = time!.minute.toString().padLeft(2, '0');
+
+      return '$hours:$minutes';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => ButtonHeaderWidget(
+        title: 'Time',
+        text: getText(),
+        onClicked: () => pickTime(context),
+      );
+
+  Future pickTime(BuildContext context) async {
+    final initialTime = TimeOfDay(hour: 9, minute: 0);
+    final newTime = await showTimePicker(
+      context: context,
+      initialTime: time ?? initialTime,
+    );
+
+    if (newTime == null) return;
+
+    setState(() => time = newTime);
+  }
+}
+
+/////////////////////////////////////////
+class ButtonHeaderWidget extends StatelessWidget {
+  final String title;
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonHeaderWidget({
+    Key? key,
+    required this.title,
+    required this.text,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => HeaderWidget(
+        title: title,
+        child: ButtonWidget(
+          text: text,
+          onClicked: onClicked,
+        ),
+      );
+}
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    Key? key,
+    required this.text,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size.fromHeight(40),
+          primary: Color.fromRGBO(236, 232, 201, 0.7),
+          shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(25.0),
+          ),
+        ),
+        child: FittedBox(
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+        ),
+        onPressed: onClicked,
+      );
+}
+
+class HeaderWidget extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  const HeaderWidget({
+    Key? key,
+    required this.title,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          child,
+        ],
+      );
+}
+
