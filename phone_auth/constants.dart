ea4e4fdfe2e2abd077d1737029bf5e09a6d6c4ed
nouf21 <90301568+nouf21@users.noreply.github.com>
2021-10-02 18:52:12 +0300

    Delete phone_auth directory

--- phone_auth/constants.dart
+++
@@ -1,9 +0,0 @@
-// ignore: unused_import
-import 'package:flutter/material.dart';
-
-// green 
-const kPrimaryColor = Color(0xFF467c54);
-// light green 
-const kPrimaryLightColor = Color(0xFFAFC051);
-// beige
-const kLightColor = Color(0xFFEDE7CD);
