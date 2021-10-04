726a4b4afd1253cd763719cbb6d589bdc79ccc46
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-10-04 22:38:47 +0300

    Add files via upload

---
+++ lib/lib/components/google_auth_api.dart
@@ -0,0 +1,24 @@
+
+
+
+
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleAuthApi{
+
+static final _googleSignIn = GoogleSignIn(scopes: ['https://mail.google.com/'],);
+
+static Future<GoogleSignInAccount> signIn() async{
+
+  if (await _googleSignIn.isSignedIn()){
+    return _googleSignIn.currentUser;
+  }else {
+    return await _googleSignIn.signIn();
+  }
+}
+
+static Future signOut()=> _googleSignIn.signOut();
+
+
+
+}
