071ea5b15c9f3de7d15a11d188b506df0325682c
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-10-09 16:22:08 +0300

    Add files via upload

--- Screens/wrapper.dart
+++ Screens/wrapper.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:test_project/Admin/adminHome.dart';
 import 'package:test_project/Admin/viewInstitution.dart';
 import 'package:test_project/Screens/authenticate/authenticate.dart';
+import 'package:test_project/Screens/authenticate/bodyLogin.dart';
 import 'package:test_project/models/userMu.dart';
 import 'package:test_project/screens/home/home.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,7 @@ import 'package:provider/provider.dart';
 import 'package:test_project/services/auth.dart';
 import 'package:test_project/shared/loading.dart';
 
-import 'Login/login_screen.dart';
+//import 'Login/login_screen.dart';
 import 'Signup/signup_screen.dart';
 import 'Welcome/welcome_screen.dart';
 
@@ -23,28 +24,14 @@ class Wrapper extends StatelessWidget {
     if (user == null){
       return Authenticate(); // was Authenticate indtead of LoginScreen  SignUpScreen
     } else {// make it else if insted of else
-    /* return FutureBuilder(
-        future: _authService.checkUserType(),
-     builder: (BuildContext context, AsyncSnapshot<Widget> widget) {
-        if(widget.connectionState == ConnectionState.done){
-          if (!widget.hasData) {
-            return Authenticate();
-          }
-          return widget.data;
-        }
-        return Center(
-          child: Text("CircularProgressIndicator"),
-        );
-      },
-     );*/
-//return ViewInstitution();
-      //return _authService.checkUserType(context);
       return new FutureBuilder(
       future: _authService.checkUserType(),
       builder: (BuildContext context, AsyncSnapshot<String> text) { // add code to wait for data
-        
-        if(text.connectionState == ConnectionState.done){
+        print("inside builder");
+        if(text.connectionState == ConnectionState.done ){
+          print("inside if connection stm");
           if (!text.hasData) {
+            print("inside hasData");
             return Loading();
           }
         
@@ -54,20 +41,43 @@ class Wrapper extends StatelessWidget {
         //return ViewInstitution();
         }
         else if (text.data == "contributor")
-        { return Home();}
-        else{ return ViewInstitution();}}
+        { print("entered home");
+          return Home();}
+        else{ return BodyLogin();}} 
       }
-      return Loading();
+      print("still loading !!!");
+      return Loading(); // يجي هنا بعد الساين اب
       }
       
       );
-    
-    }// end of else
+ /*   
+dynamic userType= checkType();
+if ( userType=="admin"){
+return AdminHome(); 
+}
+else if (userType=="contributor"){
+  print("entered home");
+          return Home();
+}
 
+    }// end of else
+return Loading();*/
   }// method build
 
     
   }
+
+
+dynamic checkType()async{
+
+AuthService _auth = AuthService();
+dynamic result= await _auth.checkUserType();
+return result;
+
+}
+
+
+
      checkRole( DocumentSnapshot snapshot) {
     if (snapshot.data == null) {
       return Center(
@@ -85,4 +95,5 @@ class Wrapper extends StatelessWidget {
  // print(userType);
   return ViewInstitution();
   }
+}
 }
