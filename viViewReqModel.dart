f34d4116574748f537a90ad122980855ae1336f6
Afnan Alzuayr <90301688+Afn4nz@users.noreply.github.com>
2021-10-16 18:47:04 +0300

    Update viViewReqModel.dart

--- viViewReqModel.dart
+++ viViewReqModel.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 // import 'package:swe444/Models/request.dart';
@@ -8,28 +9,44 @@ class ViewRequestViewModel {
 /* for ID*/
   var firebaseUser = FirebaseAuth.instance.currentUser;
 
+  /* to store docs*/
+  List itemsList = [];
+
   final CollectionReference requestList = FirebaseFirestore.instance
       .collection('contributor')
       .doc('1jJuO9tixUnDamCYf6Hx')
       .collection('request');
 
+/* To retrive requests*/
   Future getRequestList() async {
-    List itemsList = [];
-
     try {
       // Get docs from collection reference
       QuerySnapshot querySnapshot = await requestList
-          .where("status", isEqualTo: "new")
-          //        .orderBy("dateCreated")
+          //  .where("status", isEqualTo: "new")
+          //  .orderBy("dateCreated")
           .get();
+      print('I am here line 27 vi vi');
 
       querySnapshot.docs.forEach((element) {
         itemsList.add(element.data());
       });
+
       return itemsList;
     } catch (e) {
       print(e.toString());
       return null;
     }
+  }
+
+  Future cancelRequest(int index) async {
+    var docRef = FirebaseFirestore.instance
+        .collection('contributor')
+        .doc("X01gyr2qkFMP47ClkZHX")
+        .collection("request")
+        .doc();
+
+// delete the document
+    docRef.delete();
+    print('WELL DONE!!!!!!');
   }
 }
