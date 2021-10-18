c130af3ec91ea28745964621ec020267495bb858
Afnan Alzuayr <90301688+Afn4nz@users.noreply.github.com>
2021-10-18 18:58:47 +0300

    Add files via upload

---
+++ initiate.dart
@@ -0,0 +1,107 @@
+import 'package:flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+import 'package:mostadeem/Admin/viViewModel.dart';
+import 'package:mostadeem/components/google_auth_api.dart';
+import 'package:mostadeem/screens/home/home0.dart';
+import 'package:mostadeem/screens/home/viViewReqModel.dart';
+import 'package:mostadeem/screens/request.dart';
+import 'package:mostadeem/shared/loading.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:mostadeem/services/auth.dart';
+import 'package:mostadeem/components/google_auth_api.dart';
+
+import 'home/home.dart';
+
+class initiateRequest extends StatefulWidget {
+  @override
+  _initiateRequestState createState() => _initiateRequestState();
+}
+
+class _initiateRequestState extends State<initiateRequest> {
+  @override
+  @override
+  Widget build(BuildContext contexts) {
+    // ignore: unnecessary_new
+    return new Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Initiate Request'),
+        backgroundColor: Color.fromRGBO(48, 126, 80, 1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(18),
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_rounded,
+            color: Colors.white,
+          ),
+          tooltip: 'Show Snackbar',
+          onPressed: () async {
+            Navigator.of(context).pop();
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Home(),
+              ),
+            );
+          },
+        ),
+        toolbarHeight: 60.0,
+      ),
+      body: Container(
+        margin: EdgeInsets.only(top: 190, left: 15, right: 15),
+        child: Column(mainAxisSize: MainAxisSize.max,
+            //  mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Recycle today for a better tomorrow.',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Color.fromRGBO(48, 126, 80, 1),
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Icon(
+                Icons.favorite_border_rounded,
+                color: Color.fromRGBO(48, 126, 80, 1),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: SizedBox(
+                    height: 50,
+                    width: 190,
+                    child: RaisedButton(
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(25.0),
+                      ),
+                      color: Color.fromRGBO(48, 126, 80, 1),
+                      child: Text(
+                        'Start',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => requestScreen()),
+                        );
+                      },
+                    ),
+                  )),
+            ]),
+      ),
+    );
+  }
+}
