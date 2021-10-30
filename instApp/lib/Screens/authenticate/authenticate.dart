f386f77945786637ce5d0e01fba8a7d854510c30
nouf21 <90301568+nouf21@users.noreply.github.com>
2021-10-30 17:20:35 +0300

    Add files via upload

---
+++ instApp/lib/Screens/authenticate/authenticate.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:inst_app/Screens/authenticate/logIn.dart';
+import 'package:inst_app/Screens/authenticate/registerScreen.dart';
+//import 'package:test_project/Screens/Login/login_screen.dart';
+//import 'package:test_project/Screens/Signup/signup_screen.dart';
+
+
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+
+  bool showSignIn = true;
+  void toggleView(){
+    //print(showSignIn.toString());
+    setState(() => showSignIn = !showSignIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showSignIn) {
+      return logIn(toggleView: toggleView); // كانت لوق ان الكبيرة
+    } else {
+      return regScreen(toggleView:  toggleView);
+    }
+  }
+}
+
---
+++ instApp/lib/services/globals.dart
@@ -0,0 +1,7 @@
+library inst_app.globals;
+
+String userName='';
+String userPhone='';
+String userTwitter='';
+String userCateg='';
+String userEmail='';
