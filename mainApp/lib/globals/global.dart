0323c410bd7ecefa43f69e7c5845d991a04b68a4
nouf21 <90301568+nouf21@users.noreply.github.com>
2021-11-07 00:15:30 +0300

    Update global.dart

--- mainApp/lib/globals/global.dart
+++ mainApp/lib/globals/global.dart
@@ -5,21 +5,10 @@ import 'package:flutter/material.dart';
 DateTime globalDate = null;
 TimeOfDay globalTime = null;
 
-String userName = '';
-String userPhone = '';
-String userEmail = '';
 
-List<bool> reqCat = [
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false,
-  false
-];
+String userName='';
+String userPhone='';
+String userEmail='';
+int userPoints= 0;
+List<bool> reqCat= [false, false, false, false, false, false,false, false, false, false, false, false]; 
+
