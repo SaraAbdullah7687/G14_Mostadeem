e7e3944ac49292e30ae2865be7e45fa61b92a9e3
Modhi1 <90302353+Modhi1@users.noreply.github.com>
2021-11-13 23:23:09 +0300

    Add files via upload

--- lib/Admin/viViewModel.dart
+++ lib/Admin/viViewModel.dart
@@ -1,3 +1,5 @@
+//import 'dart:js';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -5,6 +7,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 class ViewInstitutionViewModel with ChangeNotifier {
   Stream<QuerySnapshot<Map<String, dynamic>>> _institutions;
+  Stream<QuerySnapshot<Map<String, dynamic>>> _instProfile;
 
   fetchInstitutions() async {
     var firebase=  FirebaseFirestore.instance
@@ -16,6 +19,14 @@ class ViewInstitutionViewModel with ChangeNotifier {
 
   Stream<QuerySnapshot<Map<String, dynamic>>> get institutions {
     return _institutions;
+  }
+
+  fetchInstProfile(String uid) async {
+    var _instProfile=  FirebaseFirestore.instance.collection('institution').doc(uid).snapshots();
+    notifyListeners();
+  }
+  Stream<QuerySnapshot<Map<String, dynamic>>> get InstProfile {
+    return _instProfile;
   }
 
 Future sendingMails(String email) async {
@@ -48,6 +59,8 @@ if (await canLaunch(url)) {
     throw 'Could not launch $url';
   }
 }
+
+
 
 
 }
