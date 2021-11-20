bbd438a5bee2788467789dfc33b9ff0afa66f5d7
nouf21 <90301568+nouf21@users.noreply.github.com>
2021-11-20 15:23:40 +0300

    Delete constants.dart

--- instApp/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color.fromRGBO(48, 126, 80, 1);//Color(0xFF33691E);//Color(0xFF6F35A5);
-const kPrimaryLightColor = Color(0xFFFFFF); // Color(0xFFF1E6FF); || Color(0XFFEDE7CD)
-
-const double padding1 = 16.0;
- const double avatarRadius1 = 66.0;
