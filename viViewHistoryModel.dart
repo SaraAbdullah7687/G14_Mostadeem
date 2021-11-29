d736689e663904db39e5f995af22fe22a2339b78
Afnan Alzuayr <90301688+Afn4nz@users.noreply.github.com>
2021-11-30 01:39:27 +0300

    Update viViewHistoryModel.dart

--- viViewHistoryModel.dart
+++ viViewHistoryModel.dart
@@ -19,8 +19,7 @@ class ViewHistoryViewModel with ChangeNotifier {
         .collection("contributor")
         .doc(uid)
         .collection("request");
-    _requests = firebase
-        .where("status", isEqualTo: ("done"))
+    _requests = firebase.where("status", whereIn: ["done", "rejected"])
         //  .orderBy("date")
         .snapshots();
     notifyListeners();
@@ -93,50 +92,4 @@ Future show(BuildContext context, Flushbar newFlushBar) async {
     flushBars.add(newFlushBar);
   }*/
 
-} // end class
-
-///////////////////
-/*
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-// import 'package:swe444/Models/request.dart';
-
-class ViewRequestViewModel {
-/* for ID*/
-  var firebaseUser = FirebaseAuth.instance.currentUser;
-
-  /* to store docs*/
-  List itemsList = [];
-
-  final CollectionReference requestList = FirebaseFirestore.instance
-      .collection('contributor')
-      .doc('HjwkryLRi4loaSunp9Pv')
-      .collection('request');
-
-/* To retrive requests*/
-  Future getRequestList() async {
-    try {
-      // Get docs from collection reference
-      QuerySnapshot querySnapshot = await requestList
-          //  .where("status", isEqualTo: "new")
-          //  .orderBy("dateCreated")
-          .get();
-      print('I am here line 27 vi vi');
-
-      querySnapshot.docs.forEach((element) {
-        itemsList.add(element.data());
-      });
-
-      return itemsList;
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
-  }
-
-
 }
-*/
