d5feb3fbd5c410aee75112556e3044e1605eaf11
nouf21 <90301568+nouf21@users.noreply.github.com>
2021-12-12 10:27:08 +0300

    Delete mainApp directory

--- mainApp/lib/models/ContributorModel.dart
+++
@@ -1,8 +0,0 @@
-class ContributorModel {
-  final String uid;
-  final String email;
-  final String name;
-  final String phone;
-
-  ContributorModel({this.uid, this.email, this.name, this.phone});
-}
--- mainApp/lib/models/emailMessage.dart
+++
@@ -1,9 +0,0 @@
-class EmailMessage{
-
-final String subject;
-final String text;
-
-EmailMessage({this.subject, this.text,});
-
-
-}
